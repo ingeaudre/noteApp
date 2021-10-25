278532515f0272c8391a3d03629d9e15cbabb671
ingeaudre <ingeaudrebarus2802@gmail.com>
2021-10-25 12:50:00 +0700

    s noteapp

---
+++ app/src/main/java/com/example/noteapp/db/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.example.noteapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.noteapp.model.Note
+
+
+@Dao
+interface NoteDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE nateTitle LIKE :query OR noteBody LIKE:query")
+    fun searchNote(query: String?): LiveData<List<Note>>
+}
---
+++ app/src/main/java/com/example/noteapp/model/Note.kt
@@ -0,0 +1,16 @@
+package com.example.noteapp.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val nateTitle:String,
+    val noteBody: String
+    ): Parcelable
